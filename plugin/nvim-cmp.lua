6370418ecba674de17e930ce8339d8bc7c22bc97
Rafał Lisowski <lisowski.r@gmail.com>
2023-09-13 13:14:36 +0200

    Configure copilot

--- plugin/nvim-cmp.lua
+++ plugin/nvim-cmp.lua
@@ -1,7 +1,9 @@
--- require("copilot").setup({
---   suggestion = { enabled = false },
---   panel = { enabled = false },
--- })
+
+require("copilot").setup({
+  suggestion = { enabled = false },
+  panel = { enabled = false },
+})
+require("copilot_cmp").setup()
 
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
@@ -23,17 +25,20 @@ if not lspkind_status_ok then
   return
 end
 
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+
 cmp.setup {
   formatting = {
     fields = { "abbr", "kind", "menu" },
-    format = function (entry, vim_item)
-      if entry.source.name == "copilot" then
-        vim_item.kind = ""
-        vim_item.kind_hl_group = "CmpItemKindCopilot"
-        return vim_item
-      end
-      return lspkind.cmp_format({ mode = 'symbol', maxwidth = 50 })(entry, vim_item)
-    end
+    format = lspkind.cmp_format({
+      mode = "symbol",
+      max_width = 50,
+      symbol_map = { Copilot = "", Text = "☷" }
+    })
   },
   snippet = {
     expand = function(args)
@@ -80,8 +85,10 @@ cmp.setup {
     },
     ["<CR>"] = cmp.mapping.confirm { select = true },
     ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
+        -- if cmp.visible() then
+        --   cmp.select_next_item()
+        if cmp.visible() and has_words_before() then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
         elseif luasnip.expandable() then
           luasnip.expand()
         elseif luasnip.expand_or_jumpable() then
