e7e9708f7f38c9e04694d9990936dfef6a3432fc
Rafał Lisowski <lisowski.r@gmail.com>
2024-01-28 23:16:54 +0100

    Remove unnecessary plugins

--- plugin/telescope.nvim.lua
+++ plugin/telescope.nvim.lua
@@ -57,7 +57,6 @@ require('telescope').setup{
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
-require("telescope").load_extension("file_browser")
 
 -- local ivy_theme = require('telescope.themes').get_ivy()
 -- ivy_theme['find_command'] = opts.find_command
--- plugin/tree-sitter.lua
+++ plugin/tree-sitter.lua
@@ -1,8 +1,12 @@
 require'nvim-treesitter.configs'.setup {
-  Highlight = {
-    enable = true
-  },
-  context_commentstring = {
-    enable = true
+  highlight = {
+    enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
 }
